9cf4dc49321cf098295af26a2f0748ef316d649b
Ben <benalexanderthegreat@gmail.com>
2024-02-15 22:59:12 +0100

    Router Layout Added

--- quizzer/src/App.js
+++ quizzer/src/App.js
@@ -1,27 +1,27 @@
-import {BrowserRouter, Routes, Route, Link, NavLink} from "react-router-dom"
-import './App.css';
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import "./App.css";
 //pages
 import Home from "./Pages/Home/Home";
 import Memory from "./Pages/Memory/Memory";
+//layout
+import RootLayout from "./layout/RootLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="/memory" element={<Memory />} />
+    </Route>
+  )
+);
 
 function App() {
-  return (
-    <BrowserRouter>
-    <header>
-      <nav>
-        <h1>Quizzer</h1>
-        <NavLink to={"/"}>Quizzer</NavLink>
-        <NavLink to={"memory"}>Games</NavLink>
-      </nav>
-    </header>
-    <main>
-      <Routes>
-        <Route index element={<Home/>} />
-        <Route path="/memory" element={<Memory/>} />
-      </Routes>
-    </main>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ quizzer/src/layout/RootLayout.js
@@ -0,0 +1,21 @@
+import { NavLink, Outlet } from "react-router-dom";
+
+const RootLayout = () => {
+  return (
+    <div className="root-layout">
+      <header>
+        <nav>
+          <h1 className="nav-header">Quizzer</h1>
+          <NavLink to={"/"}>Quizzer</NavLink>
+          <NavLink to={"memory"}>Games</NavLink>
+        </nav>
+      </header>
+    <main>
+        <Outlet />
+    </main>
+
+    </div>
+  );
+};
+
+export default RootLayout;
